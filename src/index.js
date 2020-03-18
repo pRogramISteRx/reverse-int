fc8cf3c21086ace418975cbedf9217eca756c6cc
Vadim Kotkovets <kotkowec@gmail.com>
2020-03-18 22:31:38 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+    let answer;
+    if (n < 0) {
+        n = n * (-1);
+    }
+    n = String(n);
+    answer = n.split('').reverse().join('');
+    return answer;
 }
